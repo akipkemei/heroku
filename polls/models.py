0b5d13f33b034bb2d12828140aee94982fa25faa
unknown <akipkemei@gmail.com>
2014-03-06 16:57:56 +0300

    my django app

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,8 @@ from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(default=datetime.now,blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+    #pub_date = models.DateTimeField(default=datetime.now,blank=True)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
