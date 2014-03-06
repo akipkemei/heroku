e9ecd8f4761150f168e6c645acbc6534a37d003b
unknown <akipkemei@gmail.com>
2014-03-07 01:04:18 +0300

    my django app

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -84,13 +84,13 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa/Nairobi'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
