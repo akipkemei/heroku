544db7a334b22ccb79148cf1d113a491bdf9a4e6
unknown <akipkemei@gmail.com>
2014-03-07 14:19:46 +0300

    my django app

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 #from django.utils import timezone
-from pytz import UTC
+#from pytz import UTC
 
 
 # Create your models here.
@@ -9,8 +9,8 @@ from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
-    #pub_date = models.DateTimeField(auto_now_add=True, blank=True)
-    pub_date = models.DateTimeField(default=datetime.now,blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+    #pub_date = models.DateTimeField(default=datetime.now,blank=True)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
