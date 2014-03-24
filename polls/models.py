ed2a76b145aeaee50354ea7d35e1ef6d7852de1f
unknown <akipkemei@gmail.com>
2014-03-24 15:38:15 +0300

    my django app

--- polls/models.py
+++ polls/models.py
@@ -1,18 +1,21 @@
 from django.db import models
+from django.utils import timezone
 from datetime import datetime
-#from django.utils import timezone
-#from pytz import UTC
-
-
-# Create your models here.
-from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+    #pub_date = models.DateTimeField('date published')
+    #pub_date = models.DateTimeField(timezone.now('CET'))
     #pub_date = models.DateTimeField(default=datetime.now,blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.question
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
+
+from polls.models import Poll
+
+##def index(request):
+##    return HttpResponse("Hello, world. You're at the poll index.")
+
+##def index(request):
+##    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+##    output = ', '.join([p.question for p in latest_poll_list])
+##    return HttpResponse(output)
+
+def index(request):
+    latest_poll_list = Poll.objects.all()
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
