333581e34775781e1c7193d92365fb07b75f52ba
unknown <akipkemei@gmail.com>
2014-04-01 19:04:50 +0300

    my django app

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls')),   
+    url(r'^equality/', include('equality.urls')),
+                     
   )
